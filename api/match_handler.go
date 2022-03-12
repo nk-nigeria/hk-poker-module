94869c00d234fa04b9d47d6b9a91b7a7e75782e8
DANG QUOC SON <sondq@vng.com.vn>
2022-03-12 16:21:55 +0700

    [update]: add update wallet chip by result game

--- api/match_handler.go
+++ api/match_handler.go
@@ -516,3 +516,58 @@ func (m *MatchHandler) saveCard(logger runtime.Logger, s *entity.MatchState, mes
 	s.Cards[message.GetUserId()] = cardsByClient
 	s.OrganizeCards[message.GetUserId()] = cardsByClient
 }
+
+func (m *MatchHandler) addChip(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userID string, amountChip int) {
+	changeset := map[string]int64{
+		"chips": int64(amountChip), // Add amountChip coins to the user's wallet.
+	}
+	metadata := map[string]interface{}{
+		"game_topup": "topup",
+	}
+
+	_, _, err := nk.WalletUpdate(ctx, userID, changeset, metadata, true)
+	if err != nil {
+		logger.WithField("err", err).Error("Wallet update error.")
+	}
+
+}
+
+func (m *MatchHandler) subtractChip(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, userID string, amountChip int) {
+	changeset := map[string]int64{
+		"chips": -int64(amountChip), // Substract amountChip coins to the user's wallet.
+	}
+	metadata := map[string]interface{}{
+		"game_topup": "topup",
+	}
+
+	_, _, err := nk.WalletUpdate(ctx, userID, changeset, metadata, true)
+	if err != nil {
+		logger.WithField("err", err).Error("Wallet update error.")
+	}
+}
+
+func (m *MatchHandler) updateChipByResultGameFinish(ctx context.Context, logger runtime.Logger, nk runtime.NakamaModule, resultGame *pb.UpdateFinish) {
+	walletUpdates := make([]*runtime.WalletUpdate, len(resultGame.Results))
+	for _, result := range resultGame.Results {
+		amountChip := int64(0)
+		amountChip = 200*(result.FrontFactor+result.MiddleFactor+result.BackFactor) +
+			(result.FrontBonus + result.MiddleFactor + result.BackFactor)
+		changeset := map[string]int64{
+			"chips": amountChip, // Substract amountChip coins to the user's wallet.
+		}
+		metadata := map[string]interface{}{
+			"game_topup": "topup",
+		}
+		walletUpdates = append(walletUpdates, &runtime.WalletUpdate{
+			UserID:    result.UserId,
+			Changeset: changeset,
+			Metadata:  metadata,
+		})
+	}
+
+	_, err := nk.WalletsUpdate(ctx, walletUpdates, true)
+	if err != nil {
+		logger.WithField("err", err).Error("Wallets update error.")
+	}
+
+}
